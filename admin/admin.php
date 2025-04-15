ef8ebc1d2b66618672e2facfbcc9b985065a7de5
CoockRash_WorkPC <antaNT.pro@gmail.com>
2025-04-15 14:55:29 +0300

    openserver 626

--- admin/admin.php
+++ admin/admin.php
@@ -9,18 +9,18 @@ require_once PATH_CORE . 'functions.php';
 require_once PATH_CORE . 'headers.php';
 require_once PATH_CORE . 'tables.php';
 
-$_POST   = stripslashes_deep($_POST);
-$_GET    = stripslashes_deep($_GET);
-$_COOKIE = stripslashes_deep($_COOKIE);
+$_POST   = stripslashes_deep( $_POST );
+$_GET    = stripslashes_deep( $_GET );
+$_COOKIE = stripslashes_deep( $_COOKIE );
 
 // require_once PATH_CORE . 'authentication.php';
 
-if (isset($_COOKIE['PHPSESSID'])) {
-    if (SECURITY_EXPIRE > 0) {
-        set_cookie('PHPSESSID', $_COOKIE['PHPSESSID'], time() + SECURITY_EXPIRE);
-    } else {
-        set_cookie('PHPSESSID', $_COOKIE['PHPSESSID']);
-    }
+if ( isset( $_COOKIE['PHPSESSID'] ) ) {
+	if ( SECURITY_EXPIRE > 0 ) {
+		set_cookie( 'PHPSESSID', $_COOKIE['PHPSESSID'], time() + SECURITY_EXPIRE );
+	} else {
+		set_cookie( 'PHPSESSID', $_COOKIE['PHPSESSID'] );
+	}
 }
 
 // unset($_SESSION['log']);
@@ -32,23 +32,23 @@ if (isset($_COOKIE['PHPSESSID'])) {
 
 $relaccess = checkLogin();
 
-if (! isset($_SESSION['log']) || ! in_array(100, $relaccess)) {
+if ( !isset( $_SESSION['log'] ) || !in_array( 100, $relaccess ) ) {
 
-    if (isset($_POST['user_login']) && isset($_POST['user_pw'])) {
-        // regForceSavePassword($_POST['user_login'], $_POST['user_pw']);
+	if ( isset( $_POST['user_login'] ) && isset( $_POST['user_pw'] ) ) {
+		// regForceSavePassword( $_POST['user_login'], $_POST['user_pw'] );
 
-        $hs = verifyPassword($_POST['user_login'], $_POST['user_pw']);
+		$hs = verifyPassword( $_POST['user_login'], $_POST['user_pw'] );
 
-        if ($hs) {
-            $url = set_query('&__tt=');
-            // bdump($url);
-            Redirect($url);
-        }
+		if ( $hs ) {
+			$url = set_query( '&__tt=' );
+			// bdump($url);
+			Redirect( $url );
+		}
 
-        die(ERROR_FORBIDDEN);
-    }
+		die( ERROR_FORBIDDEN );
+	}
 
-    die(ERROR_FORBIDDEN);
+	die( ERROR_FORBIDDEN );
 }
 
 //  else {
@@ -56,32 +56,32 @@ if (! isset($_SESSION['log']) || ! in_array(100, $relaccess)) {
 // }
 
 # user logout
-if (isset($_GET['logout'])) {
+if ( isset( $_GET['logout'] ) ) {
 
-    unset($_SESSION['log']);
-    unset($_SESSION['pass']);
-    unset($_SESSION['current_currency']);
+	unset( $_SESSION['log'] );
+	unset( $_SESSION['pass'] );
+	unset( $_SESSION['current_currency'] );
 
-    // RedirectJavaScript(ADMIN_FILE . '?access_deny=' . SITE_URL);
-    // RedirectJavaScript(ADMIN_FILE);
+	// RedirectJavaScript(ADMIN_FILE . '?access_deny=' . SITE_URL);
+	// RedirectJavaScript(ADMIN_FILE);
 
-    if (in_array(100, $relaccess)) {
-        Redirect('/');
-    } else {
-        // Redirect("/index.php?user_details=yes");
-        // Redirect('/index.php');
-        die(ERROR_FORBIDDEN);
-    }
+	if ( in_array( 100, $relaccess ) ) {
+		Redirect( '/' );
+	} else {
+		// Redirect("/index.php?user_details=yes");
+		// Redirect('/index.php');
+		die( ERROR_FORBIDDEN );
+	}
 
-    die(ERROR_FORBIDDEN);
+	die( ERROR_FORBIDDEN );
 }
 
 ###
 
-if (isset($_SESSION['log'])) {
-    $smarty->assign('adminlogname', $_SESSION['log']);
+if ( isset( $_SESSION['log'] ) ) {
+	$smarty->assign( 'adminlogname', $_SESSION['log'] );
 }
 //define start smarty template
-$smarty->assign('admin_main_content_template', 'start.tpl.html');
+$smarty->assign( 'admin_main_content_template', 'start.tpl.html' );
 
-$smarty->display('admin.tpl.html');
+$smarty->display( 'admin.tpl.html' );
